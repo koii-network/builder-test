9945c76667c322310e246237ec7a5b45b2d2db16
labrocadabro <labrocadabro@users.noreply.github.com>
2025-02-13 11:50:09 +0000

    Add string_utils with reverse_string function and unit tests

---
+++ string_utils.py
@@ -0,0 +1,19 @@
+def reverse_string(s: str) -> str:
+    """
+    Reverses the given string.
+    
+    Args:
+        s (str): The input string to be reversed.
+    
+    Returns:
+        str: The reversed string.
+    
+    Examples:
+        >>> reverse_string("hello")
+        'olleh'
+        >>> reverse_string("")
+        ''
+        >>> reverse_string("python")
+        'nohtyp'
+    """
+    return s[::-1]
